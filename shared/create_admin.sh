38f94075144cac07085e3ee6d947a976abdf6444
Sven Brunk <sven.brunk@oxid-esales.com>
2025-04-01 11:36:53 +0000

    changed shared scripts to reduced parameter count of OXID 8

--- shared/create_admin.sh
+++ shared/create_admin.sh
@@ -2,6 +2,6 @@ email=${ADMIN_EMAIL:-noreply@oxid-esales.com}
 password=${ADMIN_PASSWORD:-admin}
 CONSOLE_PATH=$( [ -e "source/bin/oe-console" ] && echo "bin/oe-console" || echo "vendor/bin/oe-console" )
 
-docker compose exec -T php ${CONSOLE_PATH} oe:admin:create --admin-email="$email" --admin-password="$password"
+docker compose exec -T php ${CONSOLE_PATH} oe:admin:create-user $email $password
 
 echo -e "\033[1;37m\033[1;42mCreate admin: Admin login: ${email} Password: ${password}\033[0m\n"
--- shared/setup_database.sh
+++ shared/setup_database.sh
@@ -21,19 +21,9 @@ flags "$@"
 
 echo -e "\033[1;37m\033[1;42mSetup shop\033[0m\n"
 
-# Set DIRECTORY_PATH based on the existence of /var/www/source/tmp/
-if [ -d "/var/www/source/tmp/" ]; then
-    DIRECTORY_PATH="/var/www/source/tmp"
-else
-    DIRECTORY_PATH="/var/www/var/cache"
-fi
+docker compose exec php bash -c 'mkdir -p "$OXID_BUILD_DIRECTORY"'
 
-docker compose exec -T php sh -c "if [ ! -d '${COMPILE_DIRECTORY:-${DIRECTORY_PATH}}' ]; then mkdir -p '${COMPILE_DIRECTORY:-${DIRECTORY_PATH}}'; fi"
-
-docker compose exec php ${CONSOLE_PATH} oe:setup:shop  --db-host=${MYSQL_HOST:-mysql} --db-port=${MYSQL_PORT:-3306} \
-  --db-name=${MYSQL_DATABASE:-example} --db-user=${MYSQL_USER:-root} --db-password=${MYSQL_ROOT_PASSWORD:-root} \
-  --shop-url=${SHOP_URL:-http://localhost.local/} --shop-directory=${SHOP_DIRECTORY:-/var/www/source/} \
-  --compile-directory="${COMPILE_DIRECTORY:-${DIRECTORY_PATH}}"
+docker compose exec php ${CONSOLE_PATH} oe:setup:shop
 
 $SHARED_SCRIPT_PATH/reset_database.sh
 
